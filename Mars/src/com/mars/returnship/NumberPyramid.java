0d3b2aa2cfece3620186096c468182e8e58db2a5
vinny <vinnygeoffrey@gmail.com>
2021-12-23 09:15:46 -0600

    Final commit

---
+++ Mars/src/com/mars/returnship/NumberPyramid.java
@@ -0,0 +1,19 @@
+package com.mars.returnship;
+
+public class NumberPyramid {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		int i,j=1;
+		
+		
+		for (i=1;i<=3;i++) {
+    	for(j=1;j<=i;j++) {
+    		System.out.print(j+"");
+    	}
+    	System.out.println("");
+    }
+	}
+
+}
